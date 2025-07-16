536142ea946ac4b5bd1c4055558627245b9f82e5
jamdays <venuedollar@protonmail.com>
2025-07-16 16:43:05 -0700

    moved filesys commands to seperate file

---
+++ src/filesys.h
@@ -0,0 +1,14 @@
+#include <cstddef>
+
+#ifndef FILESYS_H
+#define FILESYS_H
+
+
+static int cmd_ls (const struct shell *shell, size_t argc, char **argv); 
+
+
+static int cmd_cat(const struct shell *shell, size_t argc, char **argv);
+
+void cmd_rm(const struct shell *shell, size_t argc, char **argv); 
+
+#endif
