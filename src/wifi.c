ce542bc4f40f8fe59c769214ae5792274cbcd464
jamdays <venuedollar@protonmail.com>
2025-07-23 15:50:25 -0700

    fixed duplicate functions

--- src/wifi.c
+++ src/wifi.c
@@ -71,9 +71,6 @@ void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_e
         wifi_is_ready = true; 
     }
 }
-volatile char *ssid = NULL;
-volatile char *password = NULL;
-volatile bool wifi_is_ready = false;
 
 void read_wifi_config() {
     struct fs_file_t file;
@@ -151,69 +148,6 @@ void wifi_connect_to_saved_network() {
     } else {
         printk("No saved WiFi credentials found.\n");
     }
-}
-static int cmd_wifi_connect(const struct shell *shell, size_t argc, char **argv)
-{
-    if (argc < 3) {
-        shell_error(shell, "Usage: wifi_connect <ssid> <password>");
-        return -EINVAL;
-    }
-    wifi_params.ssid = (const uint8_t *)argv[1];
-    wifi_params.ssid_length = strlen(argv[1]);
-    wifi_params.psk = (const uint8_t *)argv[2];
-    wifi_params.psk_length = strlen(argv[2]);
-    wifi_params.security = WIFI_SECURITY_TYPE_PSK;
-    wifi_params.channel = WIFI_CHANNEL_ANY;
-
-    iface = net_if_get_default();
-    if (!iface) {
-        shell_error(shell, "Could not get default network interface");
-        return -ENODEV;
-    }
-
-    int ret = net_mgmt(NET_REQUEST_WIFI_CONNECT, iface, &wifi_params, sizeof(struct wifi_connect_req_params));
-    if (ret) {
-        shell_error(shell, "Failed to connect to WiFi: %d", ret);
-    } else {
-        shell_print(shell, "Connecting to WiFi...");
-    }
-    return ret;
-}
-
-static void cmd_wifi_save (const struct shell *shell, size_t argc, char **argv){
-    struct fs_file_t file;
-    fs_file_t_init(&file);
-    int rc = fs_open(&file, WIFI_CONFIG_FILE, FS_O_CREATE | FS_O_WRITE);
-    if (rc < 0) {
-        printk("Failed to open WiFi config file: %d\n", rc);
-        return;
-    }
-
-    if (argc == 3) {
-        fs_write(&file, argv[1], strlen(argv[1]));
-        fs_write(&file, "\n", 1);
-        fs_write(&file, argv[2], strlen(argv[2]));
-        fs_write(&file, "\n", 1);
-        printk("WiFi credentials saved to %s\n", WIFI_CONFIG_FILE);
-    } else {
-        printk("No WiFi credentials to save.\n");
-    }
-    fs_close(&file);
-}
-
-static void cmd_wifi_reconnect(const struct shell *shell, size_t argc, char **argv) {
-    wifi_connect_to_saved_network();
-}
-
-
-static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event, struct net_if *iface)
-{
-    if (mgmt_event == NET_EVENT_IPV4_ADDR_ADD) {
-        char buf[NET_IPV4_ADDR_LEN];
-        wifi_is_ready = true; 
-        //printk("IPv4 address: %s\n", net_addr_ntop(AF_INET, &iface->config.ip.ipv4->unicast[0].address.in_addr, buf, sizeof(buf)));
-    }
-
 }
 
 
